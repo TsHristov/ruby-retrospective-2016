39a77f4480145919150e0a1b8aa7899b998f2ab2
TsHristov <tsvetanhristov@yahoo.com>
2017-01-14 13:31:06 +0200

    Fix recursion problems with task 2

--- tasks/2/solution.rb
+++ tasks/2/solution.rb
@@ -3,9 +3,9 @@ class Array
     return self[keys.first.to_i] if keys.size == 1
     self[keys.first.to_i].fetch_deep(keys[1..keys.size])
   end
-  
+
   def reshape(shape)
-    self.map { |element| element.reshape(shape) }
+    map { |element| element.reshape(shape) }
   end
 end
 
@@ -14,13 +14,17 @@ class Hash
     keys = keys.split('.') if keys.is_a? String
     key = keys.first.to_sym if self.keys.include? keys.first.to_sym
     key = keys.first if self.keys.include? keys.first
+    return nil unless key
     return self[key] if keys.size == 1
     self[key].fetch_deep(keys[1..keys.size])
   end
 
   def reshape(shape)
     reshaped = {}
-    shape.each { |key, value| reshaped[key] = self.fetch_deep(value) }
+    shape.each do |key, value|
+      reshaped[key] = self.fetch_deep(value) if value.is_a? String
+      reshaped[key] = self.reshape(value)    if value.is_a? Hash
+    end
     reshaped
   end
 end
